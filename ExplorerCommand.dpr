13d32820ce715a55546b24549d565288da7021a6
vhanla@gmail.com <vhanla>
2020-04-15 22:33:57 -0500

    Single instance feature added

--- ExplorerCommand.dpr
+++ ExplorerCommand.dpr
@@ -2,6 +2,8 @@ program ExplorerCommand;
 
 uses
   Vcl.Forms,
+  Windows,
+  SysUtils,
   main in 'main.pas' {Form1},
   Vcl.Themes,
   Vcl.Styles;
@@ -9,6 +11,11 @@ uses
 {$R *.res}
 
 begin
+  if CreateMutex(nil, True, '{C97C27E2-C5FC-41BE-AF34-6C9E250FC303}') = 0 then
+    RaiseLastOSError;
+  if GetLastError = ERROR_ALREADY_EXISTS then
+    Exit;
+
   Application.Initialize;
   Application.MainFormOnTaskbar := False;
   Application.ShowMainForm := False;
